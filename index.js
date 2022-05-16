63f18f90d97133ca312c41907e4e25748ee8f1f6
Candydev0 <105285244+Candydev0@users.noreply.github.com>
2022-05-16 09:12:30 +0530

    update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const {
 const url = "https://app-cacd4d01-cdd4-4d60-88cc-acabc38c3f9f.cleverapps.io/refer/";
 const bot = new Telegraf("5332039069:AAHhFF08UCWescUeefqLWvRMYGYYuY1Wt9k");
 const setnew = (msg, refby, refst)=> {
-  var sql = `INSERT INTO users(id, fname, lname, username, balance, refer_count, refer_by, total_earned, refer_status) VALUES (${msg.message.chat.id},'${msg.message.chat.first_name}','${msg.message.chat.last_name}','${msg.message.chat.username}',0,0,${refby},0,${refst})`;
+  var sql = `INSERT INTO users(id, balance, refer_count, refer_by, total_earned, refer_status) VALUES (${msg.message.chat.id},0,0,${refby},0,${refst})`;
   mysql.query(sql, function (err, result, fields) {
   if (err) msg.reply(err);
   });
